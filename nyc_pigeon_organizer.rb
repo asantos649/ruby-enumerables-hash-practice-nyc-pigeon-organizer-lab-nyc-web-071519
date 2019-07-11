c4284b94b87fa0c573323a1379700ac3e58a28d3
Andrew Santos <asantos@alumni.nd.edu>
2019-07-11 15:05:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each {|key,value|
+    value.each{|key2, value2|
+      value2.map{|name|
+        new_hash = {name => {}}
+      }
+    }
+  #data[:color].each {|key3, value3|
+  #  counter = 0 
+  #  while value3[counter] do 
+  #    if new_hash[value3[counter]] == nil 
+  #      new_hash = {value3[counter] => value3.to_s 
+  #    end 
+  #  end 
+        }
+    return new_hash
 end
+
